d03a815482101200b12f8becc22832e3149b1d64
MarybelArista <marybelharista@gmail.com>
2022-08-27 20:57:28 -0500

    añadiendo daisyui componentes de navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <h1>HOLA DE NUEVO MUNDO!!!</h1>
-    
+    <div className='container'>
+      <NavBar/>
+    </div>
+
   );
 }
 
